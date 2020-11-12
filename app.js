a3bd39688e5196c60e8150fbffa92ac47c7ce661
Aurélien Roux <aurelien.roux@aerial.ai>
2020-11-12 11:16:39 -0500

    yargs command line argument

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const validator = require('validator')
-const chalk = require('chalk')
+// const validator = require('validator')
+// const chalk = require('chalk')
+// const fs = require('fs')
+const yargs = require('yargs')
 
-const getNotes = require('./notes')
+// Add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) =>
+    console.log(`
+    Title: ${argv.title}
+    Body: ${argv.body}
+  `)
+})
 
-const notes = getNotes()
-console.log(notes)
+// Remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => console.log('remove a note')
+})
 
-console.log(chalk.blue.bold(validator.isEmail('test@test.ca')))
-console.log(chalk.yellow(validator.isEmail('test@test.c')))
-console.log(chalk.whiteBright(validator.isEmail('test@test.c')))
-console.log(chalk.green.underline(validator.isEmail('test@test')))
-console.log(chalk.blue(validator.isURL('example.com')))
+// List command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: () => console.log('list all notes')
+})
+
+// Read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => console.log('read a note')
+})
+
+yargs.parse()
+// console.log(yargs.argv)
